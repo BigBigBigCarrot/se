67fb5e25165d08ec816c4b2cb14bb206a72acb2e
budawei <budawei>
2017-08-17 17:36:08 +0800

    注解练习

---
+++ src/com/david/annotation/PasswordUtils.java
@@ -0,0 +1,25 @@
+package com.david.annotation;
+
+import java.util.List;
+
+public class PasswordUtils {
+
+	public PasswordUtils() {
+	}
+
+	@UseCase(id=47,description="Password must contains at least on numeric")
+	public boolean validatePassword(String password){
+		return password.matches("\\w*\\d\\w*");
+	}
+	
+	@UseCase(id=48)
+	public String encryptPassword(String password){
+		return new StringBuffer(password).reverse().toString();
+	}
+	
+	@UseCase(id=49, description="New password cant not equals previous used ones")
+	public boolean checkForNewPassword(List<String> previousPassword,String password){
+		return !previousPassword.contains(password);
+	}
+	
+}
