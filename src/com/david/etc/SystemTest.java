052a9cdc98ea319b1f5b072b16637bd0483ae951
budawei <budawei>
2017-08-21 11:33:15 +0800

    System properties test

---
+++ src/com/david/etc/SystemTest.java
@@ -0,0 +1,32 @@
+package com.david.etc;
+
+import java.util.Enumeration;
+import java.util.Properties;
+
+public class SystemTest {
+
+	public SystemTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		getSystemProperties();
+//		setSystemProperty();
+	}
+
+	public static void getSystemProperties() {
+		Properties properties = System.getProperties();
+		Enumeration e = properties.propertyNames();
+		while (e.hasMoreElements()) {
+			String propertyName = e.nextElement().toString();
+			System.out.println(propertyName);
+			Object property = properties.getProperty(propertyName);
+			System.out.println(property.toString() + '\n');
+		}
+	}
+	
+	public static void setSystemProperty() {
+		System.setProperty("key1", "value1");
+		System.out.println(System.getProperty("key1"));
+	}
+}
