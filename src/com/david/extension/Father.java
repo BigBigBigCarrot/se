346e114f5ca98d31568eb52acd008de123b5af12
BigBigBigCarrot <Administrator@192.168.1.2>
2020-01-14 20:04:10 +0800

    ...

---
+++ src/com/david/extension/Father.java
@@ -0,0 +1,18 @@
+package com.david.extension;
+
+public class Father {
+
+	public Father() {
+		System.out.println("run Father()");
+	}
+	
+	public void greet() {
+		System.out.println("a greeting from father");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
