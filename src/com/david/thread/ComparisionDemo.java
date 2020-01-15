c5c144d47f229de4d37141edc35b0b31231ff622
BigBigBigCarrot <Administrator@192.168.1.2>
2020-01-15 12:09:42 +0800

    多线程示例删减

--- src/com/david/thread/ComparisionDemo.java
+++ src/com/david/thread/ComparisionDemo.java
@@ -1,12 +1,12 @@
 package com.david.thread;
 
-class Thread3 extends Thread{
+class Thread3 extends Thread{//继承Thread类，作为线程的实现类
 	private int count=5;
 	public Thread3(String name) {
-		this.setName(name);
+		this.setName(name);//通过构造方法配置name属性
 	}
 	
-	public void run() {
+	public void run() {//覆写run()方法，作为线程 的操作主体
 		for (int i = 0; i < 5; i++) {
 			System.out.println(this.getName()+"运行 count="+count--);
 			try {
@@ -43,7 +43,7 @@ public class ComparisionDemo {
 //		th2.start();
 		
 		//Runnable demostration 
-		//每个线程可以用同一个实例化对象
+		//每个线程可以用同一个实例化对象来实现共享mr的内部变量，也可以用不同的实例化对象（不能实现共享mr的内部变量）
 		MyRun2 mr=new MyRun2();
 		new Thread(mr,"C").start();
 		new Thread(mr,"D").start();
