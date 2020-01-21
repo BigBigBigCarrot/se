1ac7d197edfe82adf9f320e9f07dc7e460aa05a4
BigBigBigCarrot <Administrator@192.168.1.2>
2020-01-21 15:46:22 +0800

    MapDemo

--- src/com/david/etc/Main.java
+++ src/com/david/etc/Main.java
@@ -5,67 +5,63 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-import com.david.vo.Student;
+import org.junit.Test;
 
+import com.david.vo.Student;
 
 public class Main {
 
 	public static void main(String[] args) {
-//		f1();
-//		f2();
-//		f3();
-//		f4();
-//		f7();
-//		f8();
-		comparatorPractice();
 	}
-	
+
 	/**
 	 * 三元运算符
 	 */
-	public static void f1(){
-		int i=1;
-		System.out.println(i<2?"i<2":"i>2");
+	@Test
+	public void f1() {
+		int i = 1;
+		System.out.println(i < 2 ? "i<2" : "i>2");
 	}
-	
-	public void HashCodeBuilderPractice(){
+
+	@Test
+	public void HashCodeBuilderPractice() {
 //		HashCodeBuilder.appen("name");//org.apache.commons.lang.builder;
 	}
 
-
 	/**
-	 * This method will return
-     * null in such implementations if this class was loaded by the bootstrap
-     * class loader
+	 * This method will return null in such implementations if this class was loaded
+	 * by the bootstrap class loader
 	 */
-	public static void f2(){
+	@Test
+	public void f2() {
 		ClassLoader classLoader;
-		classLoader=String.class.getClassLoader();
+		classLoader = String.class.getClassLoader();
 		System.out.println(classLoader.getClass().getName());
 	}
-	
+
 	/**
 	 * array.getClass()
 	 */
-	public static void f3(){
+	static public void f3() {
 //		System.out.println(Student.class.getName());
 		System.out.println(Student.class.getClassLoader().getClass().getName());
 	}
-	
+
 	/**
 	 * Class<?> java.lang.Class.forName(String className)
 	 */
-	public static void f4(){
+	@Test
+	public void f4() {
 		Class cl;
-		try{
-			cl=Class.forName("java.lang.String");
-		}catch(ClassNotFoundException e){
+		try {
+			cl = Class.forName("java.lang.String");
+		} catch (ClassNotFoundException e) {
 			System.out.println(e.toString());
 			return;
 		}
 		System.out.println(cl.getName());
 	}
-	
+
 //	public static void f6(){
 //		Class<Student> cl;
 //		try{
@@ -78,35 +74,40 @@ public class Main {
 //		Student stu=cl.newInstance();
 //		System.out.println(Student.class.getClassLoader().getClass().getName());
 //	}
-	
-	public static void f7(){
+
+	@Test
+	public void f7() {
 		Class cl;
-		try{
+		try {
 //			cl=Class.forName("java.lang.String");
-			cl=Class.forName("com.david.vo.Student");
-		}catch(ClassNotFoundException e){
+			cl = Class.forName("com.david.vo.Student");
+		} catch (ClassNotFoundException e) {
 			System.out.println(e.toString());
 			return;
 		}
 		System.out.println(cl.getProtectionDomain().getCodeSource().getLocation());
 	}
-	
-	public static void f8(){
-		String qualifiedName="com.david.vo.CompanyVO";
+
+	@Test
+	public void f8() {
+		String qualifiedName = "com.david.vo.CompanyVO";
 		System.out.println(qualifiedName.replace(".", "/"));
 	}
-	
-	public static void comparatorPractice() {
-		List<Integer> list=new ArrayList();
+
+	@Test
+	public void comparatorPractice() {
+		List<Integer> list = new ArrayList();
 		list.add(5);
 		list.add(3);
 		list.add(1);
 		list.add(2);
-		Collections.sort(list);//升序排列
-		
-		//降序排列
-		Comparator comparator=new Comparator<Integer>(){//匿名内部类
-			public int compare(Integer o1, Integer o2) {return -o1.compareTo(o2);}
+		Collections.sort(list);// 升序排列
+
+		// 降序排列
+		Comparator comparator = new Comparator<Integer>() {// 匿名内部类
+			public int compare(Integer o1, Integer o2) {
+				return -o1.compareTo(o2);
+			}
 		};
 		list.sort(comparator);
 		System.out.println(list.toString());
